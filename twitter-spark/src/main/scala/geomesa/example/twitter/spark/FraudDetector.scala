d6c27ebe1cb6ec67d2512ede92b3269b6785bf95
Jim Hughes <jnh5y@ccri.com>
2016-01-12 18:19:35 -0500

    Added some comments.

--- twitter-spark/src/main/scala/geomesa/example/twitter/spark/FraudDetector.scala
+++ twitter-spark/src/main/scala/geomesa/example/twitter/spark/FraudDetector.scala
@@ -19,6 +19,7 @@ object FraudDetector {
     import scala.collection.JavaConversions._
 
     // Get a handle to the data store
+    // NB: This map is serializable, so we can pass it between Spark workers.
     val params = Map(
       "instanceId" -> Args.instance,
       "zookeepers" -> Args.zookeepers,
@@ -31,7 +32,8 @@ object FraudDetector {
 
     // Configure Spark
     val conf = new Configuration
-    val sconf = GeoMesaSpark.init(new SparkConf(true), ds)
+    // This call sets up multiple configurations including information regarding SimpleFeature serialization.
+    val sconf = GeoMesaSpark.init(new SparkConf(true), ds)  
     val sc = new SparkContext(sconf)
 
     import org.locationtech.geomesa.utils.geotools.Conversions._
@@ -42,6 +44,9 @@ object FraudDetector {
       org.locationtech.geomesa.utils.geohash.GeoHash.apply(sf.geometry.getCentroid, 25)
     }
 
+    // This computes an RDD[String, String, String] which holds the (Username, Day, GeoHash).  
+    // The day is written in the ISO Date format.  The GeoHash is a 5-character / 25-bit GeoHash.
+    // NB: No computation has occurred yet.
     val userAndGeoHash = queryRDD.mapPartitions { iter =>
       val dtf = ISODateTimeFormat.basicDate()
       iter.map { sf =>
@@ -52,6 +57,8 @@ object FraudDetector {
       }
     }
 
+    // This block computes an RDD[ (User1, User2), (Day, GeoHash) ].
+    // NB: No computation has occurred yet.
     val paired = userAndGeoHash
       .groupBy { case (u, d, g)  => (d, g) }
       .flatMap { case ( (d, g), iter) =>
@@ -64,6 +71,9 @@ object FraudDetector {
         combine.map{ case (x, y) => ( (x, y), (d, g))}
       }
 
+    // This block will for force evaluation due to its final .foreach{println}
+    //  The groupBy will pull together pairs of people.  The filter requires that those pairs appear together at least Args.minCount times.
+    //  The map transforms ( (user1, user2), (day, geohash), iter) into a like "(User1, User2)\t (day1, gh1), (day2, gh2)".
     paired
      .groupBy(_._1)
      .filter(_._2.size > Args.minCount)
